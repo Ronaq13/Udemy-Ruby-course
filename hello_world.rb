128939ea68eece8cf2e20936fd0288e8412536dc
Raounak Sharma <raounaksharma@Raounaks-MacBook-Air.local>
2022-09-26 10:08:01 +0530

    Variables in Ruby
    Understanding local and global variables

--- hello_world.rb
+++ hello_world.rb
@@ -1,1 +1,25 @@
 puts "Hello World!!"
+
+
+# Variables
+# 1. Lowercase letter or underscore
+# 2. Heterogeneous 
+# 3. Uninitialized local variables -> method calls
+# 4. Scope -> current block
+
+cars = 2
+puts cars
+
+cars = "BMW, Audi"
+puts cars
+
+
+# Global variables
+$car_company = "ABC pvt.ltd"
+puts $car_company
+
+
+# Scope ??
+
+# Class variables
+# Instance variables
