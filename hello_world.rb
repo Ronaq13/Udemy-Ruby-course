1e159fd0378b9c6eb399fdca9b8530327ea49f00
Raounak Sharma <raounaksharma@Raounaks-MacBook-Air.local>
2022-09-30 23:09:02 +0530

    Proc in ruby

--- hello_world.rb
+++ hello_world.rb
@@ -1,17 +1,28 @@
-# Lambda
+# Procs
 
-def car_running(car_number, lambda)
+def car_running(car_number, proc)
     puts "My car number is: #{car_number}"
     puts "Running in the method"
-    lambda.call(car_number)
+    proc.call
     puts "Running in the method again"
 end
 
+# Lambda
 journey = ->(car_number) do
     puts "Running in lambda with car number: #{car_number}"
     puts "Starting in the lambda"
+    return
     puts "Running in the lambda"
     puts "end of the lambda"
+end
+
+# Proc
+journey = Proc.new do |car_number|
+    puts "Running in Proc with car number: #{car_number}"
+    puts "Starting in the Proc"
+    return
+    puts "Running in the Proc"
+    puts "end of the Proc"
 end
 
 car_running(23, journey)
