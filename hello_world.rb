0c29d8082e674f8b51acb782f3a7725a70cb5c16
Raounak Sharma <raounaksharma@Raounaks-MacBook-Air.local>
2022-10-03 15:44:02 +0530

    Traversing Array

--- hello_world.rb
+++ hello_world.rb
@@ -1,30 +1,15 @@
-# Procs
+# Traversing array: each, each_with_index
 
-def car_running(car_number, proc)
-    puts "My car number is: #{car_number}"
-    puts "Running in the method"
-    proc.call
-    puts "Running in the method again"
+cars = ['Audi A4', 'BMW i8', 'Mercedes Benz']
+
+# for car in cars do 
+#     puts "I have a #{car} car"
+# end
+
+cars.each do |car|
+    puts "I have a #{car} car"
 end
 
-# Lambda
-journey = ->(car_number) do
-    puts "Running in lambda with car number: #{car_number}"
-    puts "Starting in the lambda"
-    return
-    puts "Running in the lambda"
-    puts "end of the lambda"
+cars.each_with_index do |car, index|
+    puts "I have a #{car} car. It is parked at #{index} floor"
 end
-
-# Proc
-journey = Proc.new do |car_number|
-    puts "Running in Proc with car number: #{car_number}"
-    puts "Starting in the Proc"
-    return
-    puts "Running in the Proc"
-    puts "end of the Proc"
-end
-
-car_running(23, journey)
-car_running(34, journey)
-
