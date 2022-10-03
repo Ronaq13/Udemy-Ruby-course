e7f4777ba0dc911011ce5103e7df80becad6ac6a
Raounak Sharma <raounaksharma@Raounaks-MacBook-Air.local>
2022-10-03 17:44:25 +0530

    Loops

--- hello_world.rb
+++ hello_world.rb
@@ -1,26 +1,28 @@
-# Conditional structures: if-else, unless, case
+# Loops: for, while, loop, until. 
 
 age = 25
 
-if (age > 24)
-    puts "You are an adult"
-else
-    puts "You are NOT an adult"
+while age < 50
+    puts age
+    age = age + 5
 end
 
-puts "YOu are 25 years old" if age == 25
-
-unless age > 15
-    puts "You are a child"
-else
-    puts "You are an adult"
+until age > 50
+    puts age
+    age = age + 5
 end
 
-case age
-when 15
-    puts "You are a child"
-when 25
-    puts "You are an adult"
-else
-    puts "I don't know about you"
+loop do
+    puts age
+    break if age > 50
+    age = age + 5
 end
+
+cars = ['a', 'b', 'c']
+
+for car in cars do
+    puts car
+end
+
+
+puts "Out of the loop"
