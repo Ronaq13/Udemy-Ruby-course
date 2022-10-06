16561dc584810155a4ab863ee0dc8eb84a234818
Raounak Sharma <raounaksharma@Raounaks-MacBook-Air.local>
2022-10-06 13:12:25 +0530

    Class

--- lib/models/car.rb
+++ lib/models/car.rb
@@ -1,1 +1,13 @@
-puts "Here we will put Logic to work/build with Cars"
+# Object oriented: A way thinking/seeing the world
+# Object: Something which has data and properties/methods ; data/attributes
+# Class is the blueprint and object is the instance of it.
+# 4 fundamentals of OOPs: Abstraction, Encapsulation, Inheritance, Polymorphism 
+
+class Car
+    def describe
+        puts "It has 4 wheels"
+        puts "It's colour is black"
+        puts "It's model number is 41v"
+    end
+end
+
